18ef7bec51d250cfd13bf259fa854ec3cd4a926f
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-09-01 23:38:52 -0300

    Primeiro Cubit

--- 3-jornada-flutter/6-gerencia-de-estado/counter_bloc/lib/cubit/counter_cubit_page.dart
+++ 3-jornada-flutter/6-gerencia-de-estado/counter_bloc/lib/cubit/counter_cubit_page.dart
@@ -1,10 +1,13 @@
+import 'package:counter_bloc/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubitPage extends StatelessWidget {
   const CounterCubitPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    print('Build Page');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cubit Bloc'),
@@ -13,20 +16,29 @@ class CounterCubitPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Counter 10',
-              style: Theme.of(context).textTheme.headlineMedium,
+            BlocBuilder<CounterCubit, CounterState>(
+              builder: (context, state) {
+                print('Build - CounterCubit');
+                return Text(
+                  'Counter ${state.counter}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextButton.icon(
-                  onPressed: () {},
+                  onPressed: () {
+                    context.read<CounterCubit>().increment();
+                  },
                   label: Text(''),
                   icon: Icon(Icons.add),
                 ),
                 TextButton.icon(
-                  onPressed: () {},
+                  onPressed: () {
+                    context.read<CounterCubit>().decrement();
+                  },
                   label: Text(''),
                   icon: Icon(Icons.remove),
                 ),
