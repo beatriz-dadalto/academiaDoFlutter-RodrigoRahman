ec19fcf9ae1aa807d3380009d103108f0fb5b743
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-09-01 23:38:52 -0300

    Primeiro Cubit

--- 3-jornada-flutter/6-gerencia-de-estado/counter_bloc/lib/main.dart
+++ 3-jornada-flutter/6-gerencia-de-estado/counter_bloc/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:counter_bloc/bloc/counter_bloc.dart';
 import 'package:counter_bloc/bloc/counter_bloc_page.dart';
+import 'package:counter_bloc/cubit/counter_cubit.dart';
+import 'package:counter_bloc/cubit/counter_cubit_page.dart';
 import 'package:counter_bloc/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -22,6 +24,10 @@ class MyApp extends StatelessWidget {
         '/bloc': (_) => BlocProvider(
           create: (_) => CounterBloc(),
           child: CounterBlocPage(),
+        ),
+        '/cubit': (_) => BlocProvider(
+          create: (_) => CounterCubit(),
+          child: CounterCubitPage(),
         ),
       },
       home: const HomePage(),
