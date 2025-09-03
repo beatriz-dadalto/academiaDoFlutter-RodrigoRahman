29c6347e85e7ac7e7b317736a0daafdeadc84966
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-09-02 23:20:52 -0300

    Entendendo outros widgets do bloc

--- 3-jornada-flutter/6-gerencia-de-estado/contact_bloc/lib/features/bloc_example/bloc_example.dart
+++ 3-jornada-flutter/6-gerencia-de-estado/contact_bloc/lib/features/bloc_example/bloc_example.dart
@@ -9,20 +9,89 @@ class BlocExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Bloc Example')),
-      body: BlocBuilder<ExampleBloc, ExampleState>(
-        builder: (context, state) {
-          print('--Olha--${state.runtimeType}--Aqui--');
+      body: BlocListener<ExampleBloc, ExampleState>(
+        listener: (context, state) {
+          //executa aqui apenas quando o estado muda.
+          print('-----Estado foi alterado!----BlocListener');
           if (state is ExampleStateData) {
-            return ListView.builder(
-              itemCount: state.names.length,
-              itemBuilder: (context, index) {
-                final name = state.names[index];
-                return ListTile(title: Text(name));
-              },
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('A quantidade de nomes é ${state.names.length}'),
+              ),
             );
           }
-          return Text('Nenhum nome cadastrado.');
         },
+        child: Column(
+          children: [
+            BlocConsumer<ExampleBloc, ExampleState>(
+              //BlocCosumer ele é listener e builder
+              listener: (context, state) {
+                print(
+                  '--BlocConsumer--Estado alterado para ${state.runtimeType}--',
+                );
+              },
+              builder: (_, state) {
+                if (state is ExampleStateData) {
+                  return Text('BlocConsumer | Total de nomes é ${state.names.length}');
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+            BlocSelector<ExampleBloc, ExampleState, bool>(
+              selector: (state) {
+                if (state is ExampleStateInitial) {
+                  return true;
+                }
+                return false;
+              },
+              builder: (context, showLoader) {
+                if (showLoader) {
+                  return const Expanded(
+                    child: Center(child: CircularProgressIndicator()),
+                  );
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+            SizedBox(height: 24.0,),
+            Text('Bloc Selector'),
+            BlocSelector<ExampleBloc, ExampleState, List<String>>(
+              selector: (state) {
+                if (state is ExampleStateData) {
+                  return state.names;
+                }
+                return [];
+              },
+              builder: (context, names) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: names.length,
+                  itemBuilder: (context, index) {
+                    final name = names[index];
+                    return ListTile(title: Text(name));
+                  },
+                );
+              },
+            ),
+            Text('Bloc Builder'),
+            BlocBuilder<ExampleBloc, ExampleState>(
+              builder: (context, state) {
+                print('--Olha--${state.runtimeType}--Aqui--');
+                if (state is ExampleStateData) {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: state.names.length,
+                    itemBuilder: (context, index) {
+                      final name = state.names[index];
+                      return ListTile(title: Text(name));
+                    },
+                  );
+                }
+                return Text('Nenhum nome cadastrado.');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
