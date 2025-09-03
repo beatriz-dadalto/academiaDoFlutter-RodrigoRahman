dbe310fb3fb6fc86c77d67862413d8a7251f1f4d
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-09-03 19:54:30 -0300

    Enviando parâmetros pelo evento

--- 3-jornada-flutter/6-gerencia-de-estado/contact_bloc/lib/features/bloc_example/bloc/example_bloc.dart
+++ 3-jornada-flutter/6-gerencia-de-estado/contact_bloc/lib/features/bloc_example/bloc/example_bloc.dart
@@ -9,6 +9,20 @@ part 'example_state.dart';
 class ExampleBloc extends Bloc<ExampleEvent, ExampleState> {
   ExampleBloc() : super(ExampleStateInitial()) {
     on<ExampleFindNameEvent>(_findNames);
+    on<ExampleRemoveNameEvent>(_removeName);
+  }
+
+  FutureOr<void> _removeName(
+    ExampleRemoveNameEvent event,
+    Emitter<ExampleState> emit,
+  ) {
+    final stateExample = state;
+
+    if (stateExample is ExampleStateData) {
+      final names = [...stateExample.names]; //spread para copiar a lista
+      names.retainWhere((element) => element != event.name); //quando clicar num item da lista vai remover
+      emit(ExampleStateData(names: names));
+    }
   }
 
   FutureOr<void> _findNames(
