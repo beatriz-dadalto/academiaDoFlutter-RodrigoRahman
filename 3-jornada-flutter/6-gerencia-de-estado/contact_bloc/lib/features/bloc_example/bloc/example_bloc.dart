aa326901fdfbdfcd7e4516bbdb31878e21fb5b4f
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-09-03 22:11:49 -0300

    Desafio Bloc: adiciona nome e atualiza a lista na tela

--- 3-jornada-flutter/6-gerencia-de-estado/contact_bloc/lib/features/bloc_example/bloc/example_bloc.dart
+++ 3-jornada-flutter/6-gerencia-de-estado/contact_bloc/lib/features/bloc_example/bloc/example_bloc.dart
@@ -10,6 +10,23 @@ class ExampleBloc extends Bloc<ExampleEvent, ExampleState> {
   ExampleBloc() : super(ExampleStateInitial()) {
     on<ExampleFindNameEvent>(_findNames);
     on<ExampleRemoveNameEvent>(_removeName);
+    on<ExampleAddNameEvent>(_addName);
+  }
+
+  FutureOr<void> _addName(
+    ExampleAddNameEvent event,
+    Emitter<ExampleState> emit,
+  ) {
+    final stateExample = state;
+
+    if (stateExample is ExampleStateData) {
+      final names = [...stateExample.names]; // Copia a lista atual
+      names.add(event.name); // Adiciona o novo nome
+      emit(ExampleStateData(names: names)); // Emite o novo estado
+    } else {
+      // Se não há lista ainda, cria uma nova com o primeiro nome
+      emit(ExampleStateData(names: [event.name]));
+    }
   }
 
   FutureOr<void> _removeName(
@@ -20,7 +37,9 @@ class ExampleBloc extends Bloc<ExampleEvent, ExampleState> {
 
     if (stateExample is ExampleStateData) {
       final names = [...stateExample.names]; //spread para copiar a lista
-      names.retainWhere((element) => element != event.name); //quando clicar num item da lista vai remover
+      names.retainWhere(
+        (element) => element != event.name,
+      ); //quando clicar num item da lista vai remover
       emit(ExampleStateData(names: names));
     }
   }
