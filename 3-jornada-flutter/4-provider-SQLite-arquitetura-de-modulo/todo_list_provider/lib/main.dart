37d03019c0840b27f76960bbade3cc60a5f206f8
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-10-08 05:40:54 -0300

    Update build.gradle.kts to set minSdkVersion to 23 and add repositories in buildscript

--- 3-jornada-flutter/4-provider-SQLite-arquitetura-de-modulo/todo_list_provider/lib/main.dart
+++ 3-jornada-flutter/4-provider-SQLite-arquitetura-de-modulo/todo_list_provider/lib/main.dart
@@ -1,12 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_list_provider/app/app_module.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    if (e.toString().contains(
+      'A Firebase App named "[DEFAULT]" already exists',
+    )) {
+      // já inicializado, pode ignorar
+    } else {
+      rethrow;
+    }
+  }
   runApp(AppModule());
 }
