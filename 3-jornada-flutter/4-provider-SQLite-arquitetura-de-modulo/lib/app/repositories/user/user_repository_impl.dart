62de8e9c75ca0dc5efcb504048797098a911531c
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-10-24 00:54:21 -0300

    Navegação sem Contexto e configurando home e splash

--- 3-jornada-flutter/4-provider-SQLite-arquitetura-de-modulo/lib/app/repositories/user/user_repository_impl.dart
+++ 3-jornada-flutter/4-provider-SQLite-arquitetura-de-modulo/lib/app/repositories/user/user_repository_impl.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:todo_list_provider/app/exceptions/auth_exception.dart';
 import 'package:todo_list_provider/app/repositories/user/user_repository.dart';
