fc36b4d14bf2a8d0bea1c3d33aa9f6785f3f74e8
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-10-26 18:32:52 -0300

    Alterando nome do usuário

--- 3-jornada-flutter/4-provider-SQLite-arquitetura-de-modulo/lib/app/modules/home/home_page.dart
+++ 3-jornada-flutter/4-provider-SQLite-arquitetura-de-modulo/lib/app/modules/home/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_list_provider/app/core/auth/auth_provider.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,14 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Page')),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            context.read<AuthProvider>().logout();
-          },
-          child: Text('logout'),
-        ),
-      ),
+      body: HomeDrawer(),
     );
   }
 }
---
+++ 3-jornada-flutter/4-provider-SQLite-arquitetura-de-modulo/lib/app/modules/home/widgets/home_drawer.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_overlay_loader/flutter_overlay_loader.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list_provider/app/core/auth/auth_provider.dart';
+import 'package:todo_list_provider/app/core/ui/messages.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
+import 'package:todo_list_provider/app/services/user/user_service.dart';
+
+class HomeDrawer extends StatelessWidget {
+  HomeDrawer({super.key});
+
+  final nameNotifier = ValueNotifier<String>('');
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: context.primaryColor.withAlpha(70),
+            ),
+            child: Row(
+              children: [
+                Selector<AuthProvider, String>(
+                  builder: (context, value, child) {
+                    return CircleAvatar(
+                      backgroundImage: NetworkImage(value),
+                      radius: 30.0,
+                    );
+                  },
+                  selector: (context, authProvider) {
+                    return authProvider.user?.photoURL ??
+                        'https://cdn.areademembros.com/image?p=instancia_1345%2Fimage%2FqO3OCZS8hukkR2VwxbOlpMc1d79PWN5MhLnU7ems.png&w=40&h=40&t=crop&d=default.png&i=1345&uptkn=ecc239e84bdd14272cf8a04b3601e17c';
+                  },
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Selector<AuthProvider, String>(
+                      builder: (context, value, child) {
+                        return Text(
+                          value,
+                          style: context.textTheme.titleMedium,
+                        );
+                      },
+                      selector: (context, authProvider) {
+                        return authProvider.user?.displayName ??
+                            'Nome não informado!';
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text('Alterar nome'),
+                    content: TextField(
+                      onChanged: (value) => nameNotifier.value = value,
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: Text(
+                          'Cancelar',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ),
+                      TextButton(
+                        child: Text('Alterar'),
+                        onPressed: () async {
+                          final nameValue = nameNotifier.value;
+                          if (nameValue.isEmpty) {
+                            Messages.of(context).showError('Nome obrigatório');
+                          } else {
+                            Loader.show(context);
+                            await context.read<UserService>().updateDisplayName(
+                              nameValue,
+                            );
+                            Loader.hide();
+                            Navigator.of(context).pop();
+                          }
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            title: Text('Alterar nome'),
+          ),
+          ListTile(
+            onTap: () => context.read<AuthProvider>().logout(),
+            title: Text('Sair'),
+          ),
+        ],
+      ),
+    );
+  }
+}
