f5df6845f39e0e67bdfbc9573faf69228667e2af
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-10-27 22:55:31 -0300

    Construindo Layout e animação de Transição

---
+++ 3-jornada-flutter/4-provider-SQLite-arquitetura-de-modulo/lib/app/modules/tasks/task_create_controller.dart
@@ -0,0 +1,7 @@
+
+import 'package:provider/provider.dart';
+import 'package:todo_list_provider/app/core/notifier/default_change_notifier.dart';
+
+class TaskCreateController extends DefaultChangeNotifier {
+  
+}
