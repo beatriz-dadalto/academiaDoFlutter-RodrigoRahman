843de42ee93253b0dce4e6bb4217914a9d50c92d
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-11-21 14:03:53 -0300

    modificadores de imutabilidade

---
+++ 2-dart-basico-ao-avancado/1-dart-essentials/modificadores_imutabilidade_dart/lib/consts.dart
@@ -0,0 +1,8 @@
+void main() {
+  const double pi = 3.14159; // tempo de compilacao. valor fixo apos a compilacao
+  final double piFinal = valorPI(); // tempo de execucao. final nao muda depois de atribuido
+}
+
+double valorPI() {
+  return 3.14159;
+}
---
+++ 2-dart-basico-ao-avancado/1-dart-essentials/modificadores_imutabilidade_dart/lib/final.dart
@@ -0,0 +1,12 @@
+void main() {
+  final String apiUrl = 'https://academiadoflutter.com.br/api';
+  final DateTime dataAtual = DateTime.now();
+
+  final mensagem2 = 'teste';
+
+}
+
+void mensagemQualquer(final String mensagem, String msg) {
+  // mensagem = 'batata'; ERRO pq nao pode alterar com final
+  msg = 'Bia';
+}
