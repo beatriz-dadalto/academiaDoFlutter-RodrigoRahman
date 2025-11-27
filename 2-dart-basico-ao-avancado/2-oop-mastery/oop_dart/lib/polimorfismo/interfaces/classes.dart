98b6e1d7845d0a1aae3a3f3af54b5ed499c7f47e
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-11-27 18:27:03 -0300

    Polimorfismo: interface class

---
+++ 2-dart-basico-ao-avancado/2-oop-mastery/oop_dart/lib/polimorfismo/interfaces/classes.dart
@@ -0,0 +1,5 @@
+// interface class nao pode ser extends
+// interface class pode ser implements e instanciada
+class X extends Notificador {
+
+}
---
+++ 2-dart-basico-ao-avancado/2-oop-mastery/oop_dart/lib/polimorfismo/interfaces/interfaces.dart
@@ -0,0 +1,25 @@
+// PODE SER INSTANCIADA
+// interface class eh uma classe que nao pode ser extendida fora do seu arquivo.
+// garantindo que os metodos implementados nao serao alterados!
+interface class Notificador {
+  void fazUmaRegraDeNegocioQueNaoPodeMudar() {
+    enviarNotificacao('teste');
+  }
+
+  // Nao pode ser alterado mas precisa ser publico!
+  void enviarNotificacao(String mensagem) {
+    print('Notificacao padroão');
+  }
+}
+
+// NAO PODE SER entends fora desse arquivo
+class N extends Notificador {}
+
+class N2 implements Notificador {
+  @override
+  void enviarNotificacap(String mensagem) {}
+}
+
+void main() {
+  final n = Notificador();
+}
