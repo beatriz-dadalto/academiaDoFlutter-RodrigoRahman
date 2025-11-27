db09bb21fdd230a484decbc8839c7e25daf66fa9
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-11-27 17:01:22 -0300

    Polimorfismo: abstract interface

---
+++ 2-dart-basico-ao-avancado/2-oop-mastery/oop_dart/lib/polimorfismo/interfaces/abstract_interface_exemplo.dart
@@ -0,0 +1,41 @@
+abstract interface class ProcessadorPagamento {
+  void processarPagamento(double valor);
+}
+
+abstract interface class GerarRelatorio {
+  String gerarRelatorioFechamento();
+}
+
+class SistemaPagamento implements ProcessadorPagamento, GerarRelatorio {
+  double _saldo = 0.0;
+
+  @override
+  String gerarRelatorioFechamento() {
+    return 'Relatorio: saldo atual é R\$ ${_saldo.toStringAsFixed(2)}';
+  }
+
+  @override
+  void processarPagamento(double valor) {
+    _saldo += valor;
+    print(
+      'Pagamento de R\$ ${valor.toStringAsFixed(2)} processado com sucesso',
+    );
+  }
+}
+
+void processarPagamento(ProcessadorPagamento pagamento, double valor) {
+  pagamento.processarPagamento(valor);
+}
+
+void gerarRelatorioFechamento(GerarRelatorio gerarRelatorioFechamento) {
+  print(gerarRelatorioFechamento.gerarRelatorioFechamento());
+}
+
+void main() {
+  final sistemaPagamento = SistemaPagamento();
+  processarPagamento(sistemaPagamento, 200);
+  processarPagamento(sistemaPagamento, 100.4);
+  processarPagamento(sistemaPagamento, 10.0);
+
+  gerarRelatorioFechamento(sistemaPagamento);
+}
