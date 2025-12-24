c717d2c464140c504ed0e17db47b50e0e90727df
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-12-24 09:54:26 -0300

    Anatomia do Future

---
+++ 2-dart-basico-ao-avancado/4-dart-async/projeto/lib/b_futures/c_futures.dart
@@ -0,0 +1,16 @@
+void main() {
+  //! Anatomia do Future
+  
+  // Future: Incompleto, Completo com sucesso, Completo com erro
+
+  Future(() {
+    // funcao assincrona
+  }).then((value) {
+    // registrando função que será executada quando o future estiver completo com SUCESSO
+  }).catchError((error) {
+    // registrando função que será executada quando o future estiver completo com ERRO
+  }).whenComplete(() {
+    // registrando a função que será executada SEMPRE independente quando deu erro ou sucesso
+  },);
+
+}
